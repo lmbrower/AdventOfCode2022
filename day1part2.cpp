22b5d3b4ea65180cabbb540c781586129b4c323e
lmbrower <loganbrower75@gmail.com>
2023-07-10 13:29:33 -0500

    day1part2 Solution
    Adding solution for day 1 part 2

---
+++ day1part2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream input;
+    int max, secondMax, thirdMax = 0;
+    int sum = 0;
+    int runningTotal = 0;
+    string line;
+    input.open("input1.txt");
+    while(getline(input, line)) {
+        if(line.length() == 1) {
+            if(runningTotal > thirdMax) {
+                if(runningTotal > secondMax) {
+                    if(runningTotal > max) {
+                        thirdMax = secondMax;
+                        secondMax = max;
+                        max = runningTotal;
+                    }
+                    else {
+                        thirdMax = secondMax;
+                        secondMax = runningTotal;
+                    }
+                }
+                else {
+                    thirdMax = runningTotal;
+                }
+            }
+            runningTotal = 0;
+        }
+        else {
+            runningTotal += stoi(line);
+        }
+    }
+    sum = max + secondMax + thirdMax;
+    cout << "Sum = " << sum << endl;
+    input.close();
+}
